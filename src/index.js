0643af5794fc7370fe2dd85a37a1aafc58cfa797
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-02-18 21:33:46 +0530

    initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 
 ReactDOM.render(
-    <Auth0Provider
-        domain="dev-osoluiemsmr8uoqc.us.auth0.com"
-        clientId="XZgpCG0xqLp6dpD84TWErJTKBJSXcKMh"
-        authorizationParams={{
-            redirect_uri: window.location.origin
-        }}
-    >
-        <App />
-    </Auth0Provider>,
+    <App />,
     document.getElementById("root"));
